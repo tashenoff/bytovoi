12547aa161707ce89b43770572055242d75ec30f
tashenoff <kail.dead.kd@gmail.com>
2025-04-07 17:53:51 +0600

    .

--- pages/_app.js
+++ pages/_app.js
@@ -12,20 +12,18 @@ function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
-  // Устанавливаем theme на nord
+  // Устанавливаем тему
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', 'nord');
   }, []);
 
-  // Добавление Google Analytics
+  // Google Analytics
   useEffect(() => {
-    // Создаем тег Google
     const script = document.createElement('script');
     script.async = true;
     script.src = "https://www.googletagmanager.com/gtag/js?id=AW-16964397746";
     document.head.appendChild(script);
 
-    // Инициализация gtag
     const gtagScript = document.createElement('script');
     gtagScript.innerHTML = `
       window.dataLayer = window.dataLayer || [];
@@ -36,7 +34,32 @@ function MyApp({ Component, pageProps }) {
     document.head.appendChild(gtagScript);
   }, []);
 
-  // Отслеживание маршрутов и управление состоянием загрузки
+  // Yandex.Metrika
+  useEffect(() => {
+    const yandexScript = document.createElement('script');
+    yandexScript.type = 'text/javascript';
+    yandexScript.innerHTML = `
+      (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+          if (document.scripts[j].src === r) { return; }
+        }
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+        k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(100834907, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+      });
+    `;
+    document.head.appendChild(yandexScript);
+  }, []);
+
+  // Загрузка при смене маршрута
   useEffect(() => {
     const handleStart = () => setLoading(true);
     const handleComplete = () => setLoading(false);
@@ -61,6 +84,11 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       {loading ? <LoadingSpinner /> : <Component {...pageProps} />}
+      <noscript>
+        <div>
+          <img src="https://mc.yandex.ru/watch/100834907" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+        </div>
+      </noscript>
     </>
   );
 }
